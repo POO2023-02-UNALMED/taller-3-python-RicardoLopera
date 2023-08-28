ddf93cc2ec0c866b26fb4b018174fdf89c21fbc1
Ricardo Lopera <141966162+RicardoLopera@users.noreply.github.com>
2023-08-28 08:06:01 -0500

    Taller3_Clasespedidas
    no habia guardado en VScode

---
+++ televisores/tv.py
@@ -0,0 +1,80 @@
+from televisores.marca import Marca
+from televisores.control import Control
+class TV:
+
+    numTV = 0
+
+    def __init__(self,marca,estado):
+        self._marca = marca
+        self._canal = 1
+        self._precio = 500
+        self._estado = estado
+        self._volumen = 1
+        self._control = None
+        TV.numTV += 1
+
+    def setMarca(self,marca):
+        self._marca = marca
+
+    def getMarca(self):
+        return self._marca
+    
+    def setControl(self,control):
+        self._control = control
+
+    def getControl(self):
+        return self._control
+    
+    def setPrecio(self,precio):
+        self_precio = precio
+
+    def getPrecio(self):
+        return self._precio
+    
+    @classmethod
+    def setNumTV(cls,num):
+        cls.numTV = num
+
+    @classmethod
+    def getNumTV(cls):
+        return cls.numTV
+    
+    def turnOn(self):
+        self._estado = True
+
+    def turnOff(self):
+        self._estado = False
+
+    def getEstado(self):
+        return self._estado
+    
+    def canalUp(self):
+        if (1 <= self._canal < 120) and (self._estado == True):
+            self._canal += 1
+
+    def canalDown(self):
+        if (1<self._canal <= 120) and (self._estado == True):
+            self._canal -= 1
+
+    def volumenUp(self):
+        if (0<= self._volumen<7) and (self._estado == True):
+            self._volumen += 1
+
+    def volumenDown(self):
+        if (0< self._volumen <= 7) and (self._estado == True):
+            self._volumen -= 1
+
+    def setCanal(self,canal):
+        if (1<= canal <= 120) and (self._estado == True):
+            self._canal = canal
+
+    def getCanal(self):
+        return self._canal
+    
+    def setVolumen(self,volumen):
+        if (0<= volumen <= 7) and (self._estado == True):
+            self._volumen = volumen
+            
+    def getVolumen(self):
+        return self._volumen
+    
